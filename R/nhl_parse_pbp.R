8b010d2ccb009b4cc6a48f79d95403ab0e901b34
Brock Tibert <btibert3@gmail.com>
2020-03-16 18:23:32 -0400

    skeleton for NHL parse pbp

---
+++ R/nhl_parse_pbp.R
@@ -0,0 +1,14 @@
+#' Parse NHL play by play JSON files
+#'
+#' This function parses the JSON file and returns a flattened dataframe for the plays and related data.
+#' @param gid character, length 1.  The NHL gameid
+#' @export
+#' @examples
+#' nhl_parse_pbp()
+
+
+sports_parse_pbp(gid='2019020010') {
+
+  return(TRUE)
+
+}
