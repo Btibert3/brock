83d835817a04b01351e19bc6dfc0f9fd716632c0
Brock Tibert <btibert3@gmail.com>
2020-03-17 09:37:03 -0400

    updates to parse nhl pbp

--- R/nhl_parse_pbp.R
+++ R/nhl_parse_pbp.R
@@ -8,7 +8,51 @@
 
 
 sports_parse_pbp(gid='2019020010') {
+  # url
+  URL = 'http://statsapi.web.nhl.com/api/v1/game/%s/feed/live'
+  URL = sprintf(URL, gid)
+  # fail function
+  fail_function = function() {
+    return(data.frame())
+  }
+  # get the pbp data for the gid
+  events = jsonlite::fromJSON(URL)
+  # fail gracefully as a dataframe
+  # TODO: better way?
+  if (! 'copyright' %in% names(events)) {
+    fail_function()
+  }
+  if (length(events) < 1) {
+    fail_function()
+  }
+  # filter down to the plays
+  plays = events$liveData$plays$allPlays
+  plays2 = jsonlite::flatten(plays)
 
-  return(TRUE)
+  # extract players to flatten
+  players = plays2$players
+  plays2$players = NULL
+
+  # for each entry, if players, for player in , 1 row dataframe
+  flatten_players = function(x) {
+    if (length(x) == 0) {
+      return(data.frame)
+  }
+
+  # alskdjf
+
+
+
+  }
+  # add to plays2
+  # clean . in names with _
+
+  # iterate over each
+
+  # --------- result
+  test = 'result'
+
+
+
 
 }
